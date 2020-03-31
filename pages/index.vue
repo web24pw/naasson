a2e41cf8efcd5d164fe28b6f534054f20b92b601
Anton_Gonzo <andreyevanton@mail.ru>
2020-03-31 16:13:49 +0300

    container

--- pages/index.vue
+++ pages/index.vue
@@ -40,13 +40,6 @@ export default {
 </script>
 
 <style>
-  .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
 
   .title {
     font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
